ff078be99739bc9f0dc274f1b997c12650e79aac
Chemdude8 <chemdude8@hotmail.com>
2026-05-30 01:28:18 -0600

    gungame updates

--- casino.lua
+++ casino.lua
@@ -17,11 +17,18 @@ local disabledUnits = {
 	["armmlv"] = true
 }
 
+local shipUnitsByMetalCost = {}
+
 for name, unitDef in pairs(UnitDefs) do
         if not disabledUnits[name] and unitDef.metalcost and unitDef.metalcost > 10 then
 		    table.insert(randomUnitsStringByMetalCostTable, name)
-			for i = 1, (50000 / unitDef.metalcost) do
-                table.insert(randomUnitsStringByMetalCostTable, name)
+			for i = 1, math.max(40, (50000 / unitDef.metalcost)) do
+                -- check for ships:
+                if (unitDef and unitDef.customparams and unitDef.customparams.subfolder and string.find(unitDef.customparams.subfolder:lower(), "ship", 1, true)) then
+                    table.insert(shipUnitsByMetalCost, name) 
+                else
+                    table.insert(randomUnitsStringByMetalCostTable, name)
+                end
 			end
         end
 end
@@ -29,7 +36,7 @@ end
 
 randomUnitsStringByMetalCostString = table.concat(randomUnitsStringByMetalCostTable, " ")
 
-print("woot2")
+print("woot33")
 print(randomUnitsStringByMetalCostString)
 
 coms = {"corcom", "armcom", "legcom"}
@@ -40,7 +47,13 @@ for _, i in pairs(coms) do
 				badtargetcategory = "VTOL GROUNDSCOUT SHIP",
 				def = "BOTCANNON",
 				onlytargetcategory = "NOTSHIP",
-			}}
+			},
+            --[[ [2] = {
+				badtargetcategory = "VTOL GROUNDSCOUT",
+				def = "SHIPCANNON",
+				onlytargetcategory = "ALL",
+			}]]
+                }
         unitDef.weapondefs = {
 		botcannon = {
 				accuracy = 0.2,
@@ -95,7 +108,61 @@ for _, i in pairs(coms) do
 					default = 0,
 					shields = 250,
 				},
-			}
+			},
+            --[[ shipcannon = {
+				accuracy = 0.2,
+				areaofeffect = 10,
+				avoidfeature = false,
+				avoidfriendly = false,
+				burst = 2,
+				burstrate = 0.1,
+				collidefriendly = false,
+				craterareaofeffect = 116,
+				craterboost = 0.1,
+				cratermult = 0.1,
+				edgeeffectiveness = 0.15,
+				energypershot = 100,
+				explosiongenerator = "custom:botrailspawn",
+				gravityaffected = "true",
+				heightboostfactor = 8,
+				hightrajectory = 1,
+				impulsefactor = 0.5,
+				leadbonus = 0,
+				model = "LegionUnitCapsule.s3o",
+				movingaccuracy = 600,
+				mygravity = 4.8,
+				name = "Long range bot cannon",
+				noselfdamage = true,
+				range = 500,
+				reloadtime = 0.9,
+				sprayangle = 2800,
+				stockpile = true,
+				stockpiletime = 4,
+				soundhit = "xplonuk1xs",
+				soundhitwet = "splshbig",
+				soundstart = "lrpcshot3",
+				soundstartvolume = 50,
+				turret = true,
+				trajectoryheight = 1,
+				waterbounce = true,
+				bounceSlip = 0.74,
+				bouncerebound = 0.5,
+				numbounce = 10,
+				weapontype = "Cannon",
+				weaponvelocity = 2000,
+				customparams = {
+					spawns_name = shipUnitsByMetalCost,
+					spawns_expire = 25000,
+					spawns_surface = "SEA", -- Available: "LAND SEA"
+					spawns_mode = "random",
+					stockpilelimit = 10,
+					weapons_group = 2,
+				},
+				damage = {
+					default = 0,
+					shields = 250,
+				},
+			} ]]
 		}
 end
 
--- gungame.lua
+++ gungame.lua
@@ -1,77 +1,20 @@
 -- gungame v1
 -- no need to copy since I'm not making new unit types altogether UnitDefs.armflea = table.copy(UnitDefs.armpw)
-local evolveOrder = {"armpw","corstorm", "armham", "legcen", "legbar", "cormort", "cortrem", "armsptk", "corkorg" }
- -- corkorg jugg to finish
+local evolveOrder = {{unitname= "armpw", evolvexpmod= 0.03},{unitname= "corstorm", evolvexpmod= 0.06}, {unitname= "armham", evolvexpmod= 0.09},
+ {unitname= "legcen", evolvexpmod= 0.12},{unitname= "legbar", evolvexpmod= 0.15}, {unitname= "cormort", evolvexpmod= 0.18}, {unitname= "cortrem", evolvexpmod= 0.20},
+ {unitname= "armsptk", evolvexpmod= 0.35}, {unitname= "legeheatraymech", evolvexpmod= 1}}
 local unitDef
 
-UnitDefs["corkorg"].autoheal=100000
-UnitDefs["corkorg"].sightdistance=100000
-UnitDefs["corkorg"].radardistance=100000
+-- Ending unit increase AA to clear spawners
+UnitDefs["legeheatraymech"].autoheal=100000
+UnitDefs["legeheatraymech"].sightdistance=100000
+UnitDefs["legeheatraymech"].radardistance=100000
+UnitDefs["legeheatraymech"].weapondefs["legflak_gun"].range = 10000
+UnitDefs["legeheatraymech"].weapondefs["legflak_gun"].reloadtime = 0.5
+UnitDefs["legeheatraymech"].weapondefs["legflak_gun"].damage.vtol = 10000
 
-UnitDefs["corkorg"].weapondefs['cor_advsam'] = {
-				areaofeffect = 425,
-				avoidfeature = false,
-				avoidfriendly = false,
-				burnblow = true,
-				canattackground = false,
-				castshadow = false,
-				cegtag = "missiletrailaa-large",
-				collidefriendly = false,
-				craterareaofeffect = 425,
-				craterboost = 0,
-				cratermult = 0,
-				edgeeffectiveness = 0.75,
-				energypershot = 1,
-				explosiongenerator = "custom:genericshellexplosion-huge-aa",
-				firestarter = 90,
-				flighttime = 2.2,
-				impulsefactor = 0,
-				metalpershot = 0,
-				model = "corscreamermissile.s3o",
-				name = "Long-range heavy g2a missile launcher",
-				noselfdamage = true,
-				proximitypriority = -1,
-				range = 2400,
-				reloadtime = 0.1,
-				smokecolor = 0.9,
-				smokeperiod = 2,
-				smokesize = 9,
-				smoketime = 20,
-				smoketrail = true,
-				smoketrailcastshadow = false,
-				soundhit = "impact",
-				soundhitvolume = 8,
-				soundhitwet = "splslrg",
-				soundstart = "aarocket",
-				soundstartvolume = 8,
-				sprayangle = 10000,
-				startvelocity = 1400,
-				stockpile = false,
-				stockpiletime = 14,
-				texture1 = "null",
-				texture2 = "smoketrailaaflak",
-				tolerance = 10000,
-				tracks = true,
-				trajectoryheight = 0.55,
-				turnrate = 99000,
-				turret = true,
-				weaponacceleration = 1000,
-				weapontype = "MissileLauncher",
-				weaponvelocity = 1850,
-				customparams = {
-					stockpilelimit = 5,
-				},
-				damage = {
-					vtol = 99999,
-				},
-			}
-UnitDefs["corkorg"].weapons[#UnitDefs["corkorg"].weapons+1] =  {
-				badtargetcategory = "NOTAIR",
-				def = "COR_ADVSAM",
-				onlytargetcategory = "VTOL",
-		}
-
-
+UnitDefs["legeheatraymech"].weapondefs["heatray1"].range = 10000
+UnitDefs["legeheatraymech"].weapondefs["heatray1"].energypershot = 0.1
 
 
 local function unit(name)
@@ -79,89 +22,98 @@ local function unit(name)
 	return unitDef
 end
 
-UnitDefs["ggeff"] = table.copy(UnitDefs["comeffigylvl1"])
+-- UnitDefs["ggeff"] = table.copy(UnitDefs["comeffigylvl1"])
 for i=1 , #evolveOrder - 1 do
 	
-	unit(evolveOrder[i])
+	unit(evolveOrder[i].unitname)
 	
-		UnitDefs[evolveOrder[i].."eff"] = table.copy(UnitDefs[ "corfink" ])
-		--UnitDefs[evolveOrder[i].."eff"] = table.copy(UnitDefs[ evolveOrder[i] ])
-		UnitDefs[evolveOrder[i].."eff"].customparams.iseffigy = true
-		UnitDefs[evolveOrder[i].."eff"].sightdistance = 0
-		UnitDefs[evolveOrder[i].."eff"].radardistance = 0
-		UnitDefs[evolveOrder[i].."eff"].weapondefs = {}
-        UnitDefs[evolveOrder[i].."eff"].cruisealtitude = 400
-		UnitDefs[evolveOrder[i].."eff"].health = 1000
+		UnitDefs[evolveOrder[i].unitname.."eff"] = table.copy(UnitDefs[ "armhvytrans" ])
+		--UnitDefs[evolveOrder[i].unitname.."eff"] = table.copy(UnitDefs[ evolveOrder[i].unitname ])
+		UnitDefs[evolveOrder[i].unitname.."eff"].customparams.iseffigy = true
+		UnitDefs[evolveOrder[i].unitname.."eff"].sightdistance = 0
+		UnitDefs[evolveOrder[i].unitname.."eff"].radardistance = 0
+		UnitDefs[evolveOrder[i].unitname.."eff"].weapondefs = {}
+        UnitDefs[evolveOrder[i].unitname.."eff"].cruisealtitude = 400
+		-- make it healthy against everything but the gameender
+		UnitDefs[evolveOrder[i].unitname.."eff"].health = 10000
+		-- set low metal cost since that impacts the xp given
+		UnitDefs[evolveOrder[i].unitname.."eff"].metalcost = 1
+		-- hopefully makes it fly on start?
+		UnitDefs[evolveOrder[i].unitname.."eff"].activatewhenbuilt = true
 		-- UnitDefs["zggeff"..i] = table.copy(UnitDefs["comeffigylvl1"])
         
 
         -- effigy theories
 		
 		--[[
-        UnitDefs[evolveOrder[i].."eff"].maxacc = 0.0
-		UnitDefs[evolveOrder[i].."eff"].activatewhenbuilt = true
-		UnitDefs[evolveOrder[i].."eff"].autoheal = 1000
-		UnitDefs[evolveOrder[i].."eff"].maxdec = 0
-		UnitDefs[evolveOrder[i].."eff"].builddistance = 145
-		UnitDefs[evolveOrder[i].."eff"].buildpic = "COMEFFIGY.DDS"
-		UnitDefs[evolveOrder[i].."eff"].buildtime = 6000
-		UnitDefs[evolveOrder[i].."eff"].cancloak = true
-		UnitDefs[evolveOrder[i].."eff"].canmove = false
-		UnitDefs[evolveOrder[i].."eff"].canselfdestruct = true
-		UnitDefs[evolveOrder[i].."eff"].capturable = false
-		UnitDefs[evolveOrder[i].."eff"].cloakcost = 0
-	    UnitDefs[evolveOrder[i].."eff"].cloakcostmoving = 0
-		UnitDefs[evolveOrder[i].."eff"].cloaktimeout = 0
-		UnitDefs[evolveOrder[i].."eff"].collisionvolumeoffsets = "0 3 0"
-		UnitDefs[evolveOrder[i].."eff"].collisionvolumescales = "0 0 0"
-		UnitDefs[evolveOrder[i].."eff"].collisionvolumetype = "CylY"
-		UnitDefs[evolveOrder[i].."eff"].corpse = "HEAP"
-		UnitDefs[evolveOrder[i].."eff"].explodeas = "decoycommander"
-		UnitDefs[evolveOrder[i].."eff"].footprintx = 2
-		UnitDefs[evolveOrder[i].."eff"].footprintz = 2
-		UnitDefs[evolveOrder[i].."eff"].hidedamage = false
-    	UnitDefs[evolveOrder[i].."eff"].holdsteady = true
-		UnitDefs[evolveOrder[i].."eff"].initcloaked = false
-		UnitDefs[evolveOrder[i].."eff"].sightemitheight = 40
-		UnitDefs[evolveOrder[i].."eff"].mass = 1
-		UnitDefs[evolveOrder[i].."eff"].health = 9999
-        UnitDefs[evolveOrder[i].."eff"].weapons = {}
+        UnitDefs[evolveOrder[i].unitname.."eff"].maxacc = 0.0
+		UnitDefs[evolveOrder[i].unitname.."eff"].activatewhenbuilt = true
+		UnitDefs[evolveOrder[i].unitname.."eff"].autoheal = 1000
+		UnitDefs[evolveOrder[i].unitname.."eff"].maxdec = 0
+		UnitDefs[evolveOrder[i].unitname.."eff"].builddistance = 145
+		UnitDefs[evolveOrder[i].unitname.."eff"].buildpic = "COMEFFIGY.DDS"
+		UnitDefs[evolveOrder[i].unitname.."eff"].buildtime = 6000
+		UnitDefs[evolveOrder[i].unitname.."eff"].cancloak = true
+		UnitDefs[evolveOrder[i].unitname.."eff"].canmove = false
+		UnitDefs[evolveOrder[i].unitname.."eff"].canselfdestruct = true
+		UnitDefs[evolveOrder[i].unitname.."eff"].capturable = false
+		UnitDefs[evolveOrder[i].unitname.."eff"].cloakcost = 0
+	    UnitDefs[evolveOrder[i].unitname.."eff"].cloakcostmoving = 0
+		UnitDefs[evolveOrder[i].unitname.."eff"].cloaktimeout = 0
+		UnitDefs[evolveOrder[i].unitname.."eff"].collisionvolumeoffsets = "0 3 0"
+		UnitDefs[evolveOrder[i].unitname.."eff"].collisionvolumescales = "0 0 0"
+		UnitDefs[evolveOrder[i].unitname.."eff"].collisionvolumetype = "CylY"
+		UnitDefs[evolveOrder[i].unitname.."eff"].corpse = "HEAP"
+		UnitDefs[evolveOrder[i].unitname.."eff"].explodeas = "decoycommander"
+		UnitDefs[evolveOrder[i].unitname.."eff"].footprintx = 2
+		UnitDefs[evolveOrder[i].unitname.."eff"].footprintz = 2
+		UnitDefs[evolveOrder[i].unitname.."eff"].hidedamage = false
+    	UnitDefs[evolveOrder[i].unitname.."eff"].holdsteady = true
+		UnitDefs[evolveOrder[i].unitname.."eff"].initcloaked = false
+		UnitDefs[evolveOrder[i].unitname.."eff"].sightemitheight = 40
+		UnitDefs[evolveOrder[i].unitname.."eff"].mass = 1
+		UnitDefs[evolveOrder[i].unitname.."eff"].health = 9999
+        UnitDefs[evolveOrder[i].unitname.."eff"].weapons = {}
 		]]
 
 
 		
 
 		-- unitDef.builder = true
-		-- unitDef.customparams.iscommander = true
-        unitDef.customparams.evolution_target = evolveOrder[i+1]
+		unitDef.customparams.iscommander = true
+        unitDef.customparams.evolution_target = evolveOrder[i+1].unitname
         unitDef.customparams.evolution_condition = "xp"
 		unitDef.customparams.inheritxpratemultiplier = 0.0
         -- may need to move the xp threshold into the unit list...
-        unitDef.customparams.evolution_xp_threshold = 0.02 * i
+        unitDef.customparams.evolution_xp_threshold = evolveOrder[i].evolvexpmod
         unitDef.customparams.respawn_condition = "health"
 		-- try using one effigy as the effigy for all units?
 	    -- unitDef.customparams.effigy = "ggeff"
-        unitDef.customparams.effigy = evolveOrder[i].."eff"
+        unitDef.customparams.effigy = evolveOrder[i].unitname.."eff"
 		
 		unitDef.customparams.respawn_health_threshold = 0
-        unitDef.customparams.respawn_pad = true
+        unitDef.customparams.respawn_pad = 1
         unitDef.customparams.effigy_offset = 1
         unitDef.customparams.minimum_respawn_stun = 0
         unitDef.customparams.distance_stun_multiplier = 0
         unitDef.customparams.respawn_health = unitDef.health
         unitDef.customparams.respawn_stun_duration = 0
-		unitDef.customparams.destructive_respawn = 4
+		-- how many times you get to attempt a given level
+		unitDef.customparams.destructive_respawn = 20
 end
 
 
 coms = {"corcom", "armcom", "legcom"}
 for _, i in pairs(coms) do
 	unit(i)
+		if (unitDef) then
 		unitDef.weapons = {}
         unitDef.weapondefs = {}
         unitDef.customparams.evolution_target = "armpw"
+		-- unitDef.customparams.evolution_target ="corkorg"
         unitDef.customparams.evolution_condition = "xp"
         unitDef.customparams.evolution_xp_threshold = 0
+		end
 end
 
 Spring.Echo("Welcome to gun game!")
